d0eb71ee160ce6e65d0b205d198916f38688c8d9
viza-403 <163036645+vizakan10@users.noreply.github.com>
2025-03-03 23:07:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def delete_last_user():
         return jsonify({"error": "Failed to delete the last user"}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
